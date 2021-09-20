c5e1210db37086cfe8ba29f982095d614c67a6a0
Naem Haddad <naemhd@Naems-MacBook-Air.local>
2021-09-20 18:42:51 -0500

    init

---
+++ src/main/java/com/waa/ecommerc/EcommercApplication.java
@@ -0,0 +1,13 @@
+package com.waa.ecommerc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommercApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcommercApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/waa/ecommerc/EcommercApplicationTests.java
@@ -0,0 +1,13 @@
+package com.waa.ecommerc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommercApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
